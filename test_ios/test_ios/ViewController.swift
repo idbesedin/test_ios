9f78888d1fd99c0eff278ef7d4469bc11ba80035
Besedin Ivan <ivanbesedin.05@gmail.com>
2023-10-09 18:57:50 +0300

    added project

---
+++ test_ios/test_ios/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  test_ios
+//
+//  Created by Ivan on 02.10.2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
